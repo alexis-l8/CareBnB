c797c5655e4e79fbe48666afe4860560a156272a
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-06 19:00:25 +0100

    User's location is centered on map The user's location is the center of the google map now. I have also changed the button into an input with type submit. The input's have also been given a name attribute. Relates to issue #26

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
-function initMap() {
-  const locations = [
+/* eslint-disable */
+var fetch = function(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        cb(null, JSON.parse(xhr.responseText));
+      } else {
+        cb(new Error('Api request failed'));
+      }
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+};
+
+function initMap(err, res) {
+  if (err) throw err;
+  var userLatitude= res.result.latitude;
+  var userLongitude = res.result.longitude;
+
+
+  var locations = [
     ['Barbara B.', 51.532131, -0.038538, 1],
     ['Marjorie S.', 51.530649, -0.038366, 2],
     ['Harry D.', 51.529501, -0.038152, 3],
@@ -7,16 +28,15 @@ function initMap() {
   ];
 
   // CREATES BASIC GOOGLE MAP
-  const map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: new google.maps.LatLng(51.529506, -0.042201),
+    center: new google.maps.LatLng(userLatitude, userLongitude),
   });
 
   // ? WINDOW INFORMATION OBJECT
-  const infowindow = new google.maps.InfoWindow();
+  var infowindow = new google.maps.InfoWindow();
 
-  let marker;
-  let i;
+  var marker, i;
 
   for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
@@ -30,4 +50,12 @@ function initMap() {
       infowindow.open(map, marker);
     })(marker, i));
   }
+}
+
+
+function createMap() {
+  var params = (new URLSearchParams(location.search.slice(1)));
+  var postcode = params.get('postcode').replace(' ', '%20');
+
+  fetch('https://api.postcodes.io/postcodes/'+postcode, initMap);
 }
