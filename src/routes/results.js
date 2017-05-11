750387a1d0dea3ec49c55790059707082413e9b9
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-11 08:57:58 +0100

    Added Joi Validation I've added Joi validation which makes sure a valid postcode and distance is given by the user. Relates to issue #54

--- src/routes/results.js
+++ src/routes/results.js
@@ -1,20 +1,19 @@
 const data = require('./../database/get_data');
-const querystring = require('querystring');
+const joi = require('joi');
 
 module.exports = {
-  method: 'GET',
+  method: 'POST',
   path: '/results',
+  config: {
+    validate: {
+      payload: joi.object({
+        postcode: joi.string().regex(/^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/).required(),
+        distance: joi.number().integer().min(5).max(15).required(),
+      }),
+    },
+  },
   handler: (request, reply) => {
-    const url = `${request.connection.info.protocol
-     }://${
-     request.info.host
-     }${request.url.path}`;
-
-    const params = querystring.parse(url);
-    const distance = params[Object.keys(params)[0]];
-    const postcode = params[Object.keys(params)[1]].replace(' ', '+');
-    request.cookieAuth.set({ postcode, distance });
-    console.log(request.auth.credentials);
+    request.cookieAuth.set(request.payload);
     data.getHomeOwners((dbErr, res) => {
       if (dbErr) {
         reply('Sorry, there is a database connection error');
